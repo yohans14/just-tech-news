eaf795abedba281ee2bea0992df12b25eab60712
yohans14 <yohans14@yahoo.com>
2022-02-15 22:16:11 -0600

    crated dashbord

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -92,12 +92,12 @@ router.get("/:id", (req, res) => {
     });
 });
 
-router.post("/", (req, res) => {
+router.post("/",  (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
     title: req.body.title,
     post_url: req.body.post_url,
-    user_id: req.body.user_id,
+    user_id: req.session.user_id,
   })
     .then((dbPostData) => res.json(dbPostData))
     .catch((err) => {
