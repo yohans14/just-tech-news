52fcc9fb8962a5037e80f1e9fa8771b3b832510a
yohans14 <yohans14@yahoo.com>
2022-02-16 02:44:09 -0600

    created an edit post page

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+
 const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes.js");
 const dashboardRoutes = require("./dashboard-routes.js");
@@ -6,9 +7,5 @@ const dashboardRoutes = require("./dashboard-routes.js");
 router.use("/api", apiRoutes);
 router.use("/", homeRoutes);
 router.use("/dashboard", dashboardRoutes);
-
-router.use((req, res) => {
-  res.status(404).end();
-});
 
 module.exports = router;
